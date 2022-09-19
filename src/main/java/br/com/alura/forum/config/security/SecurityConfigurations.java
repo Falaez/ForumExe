194e9671d2d7eded1857c2da78255f18deb3cd75
Mario <mariohenriquelp@gmail.com>
2022-09-19 14:27:28 -0300

    Autenticando usuario

--- src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -1,25 +1,42 @@
 package br.com.alura.forum.config.security;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+@SuppressWarnings("deprecation")
 @EnableWebSecurity
 @Configuration
-public class SecurityConfigurations{
+public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
 	
-	@Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests()
-        .antMatchers(HttpMethod.GET, "/topicos").permitAll()
-        .antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
-        .anyRequest().authenticated()
-        .and().formLogin();
-        return http.build();
-    }
-
+	@Autowired
+	private AutenticacaoService autenticacaoService;
 	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+		auth.userDetailsService(autenticacaoService).passwordEncoder(new BCryptPasswordEncoder());
+	}
+	
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+	 http.authorizeHttpRequests()
+     .antMatchers(HttpMethod.GET, "/topicos").permitAll()
+     .antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
+     .anyRequest().authenticated()
+     .and().formLogin();
+	}
+	   
+	
+	
+	
+	
+    
 }
+	
+
